dccf63e158b031500c25dab7de6da44f571c870a
mohamed.attalah <m3atallah@gmail.com>
2018-04-12 10:53:13 +0200

    Done

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,16 +1,16 @@
-import { Inject } from '@angular/core';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import { DisplayMessage } from '../shared/models/display-message';
-import { Subscription } from 'rxjs/Subscription';
+import {Inject} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router, ActivatedRoute} from '@angular/router';
+import {DisplayMessage} from '../shared/models/display-message';
+import {Subscription} from 'rxjs/Subscription';
 import {
   UserService,
   AuthService
 } from '../service';
 
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
+import {Subject} from 'rxjs/Subject';
 
 @Component({
   selector: 'app-signup',
@@ -19,7 +19,7 @@ import { Subject } from 'rxjs/Subject';
 })
 export class SignupComponent implements OnInit, OnDestroy {
   title = 'Sign up';
-  githubLink = 'https://github.com/bfwg/angular-spring-starter';
+  githubLink = '';
   form: FormGroup;
 
   /**
@@ -38,28 +38,26 @@ export class SignupComponent implements OnInit, OnDestroy {
   returnUrl: string;
   private ngUnsubscribe: Subject<void> = new Subject<void>();
 
-  constructor(
-    private userService: UserService,
-    private authService: AuthService,
-    private router: Router,
-    private route: ActivatedRoute,
-    private formBuilder: FormBuilder
-  ) {
+  constructor(private userService: UserService,
+              private authService: AuthService,
+              private router: Router,
+              private route: ActivatedRoute,
+              private formBuilder: FormBuilder) {
 
   }
 
   ngOnInit() {
     this.route.params
-    .takeUntil(this.ngUnsubscribe)
-    .subscribe((params: DisplayMessage) => {
-      this.notification = params;
-    });
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe((params: DisplayMessage) => {
+        this.notification = params;
+      });
     // get return url from route parameters or default to '/'
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     this.form = this.formBuilder.group({
       username: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(64)])],
       password: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(32)])],
-      firstname:[''],
+      firstname: [''],
       lastname: ['']
     });
   }
@@ -82,19 +80,19 @@ export class SignupComponent implements OnInit, OnDestroy {
 
     this.authService.signup(this.form.value)
     // show me the animation
-    .delay(1000)
-    .subscribe(data => {
-      console.log(data);
-      this.authService.login(this.form.value).subscribe(data =>{
-        this.userService.getMyInfo().subscribe();
-      })
-      this.router.navigate([this.returnUrl]);
-    },
-    error => {
-      this.submitted = false;
-      console.log("Sign up error" + JSON.stringify(error));
-      this.notification = { msgType: 'error', msgBody: error['error'].errorMessage };
-    });
+      .delay(1000)
+      .subscribe(data => {
+          console.log(data);
+          this.authService.login(this.form.value).subscribe(data => {
+            this.userService.getMyInfo().subscribe();
+          })
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.submitted = false;
+          console.log("Sign up error" + JSON.stringify(error));
+          this.notification = {msgType: 'error', msgBody: error['error'].errorMessage};
+        });
 
   }
 
